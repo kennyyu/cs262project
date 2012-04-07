7915264c102a17a6c3d8ec4d58d15d9317fb63d9
Jim Danz <danz@fas.harvard.edu>
2012-04-07 15:27:48 -0400

    added a stub and some psuedocode to SubmisionStorageService

--- src/edu/harvard/cs262/grading/SubmissionStorageService.java
+++ src/edu/harvard/cs262/grading/SubmissionStorageService.java
@@ -4,5 +4,11 @@ package edu.harvard.cs262.grading;
  * Service for persistent storage of student work submission.
  */
 public interface SubmissionStorageService {
-
+	/*
+	bool put(studentID, workID, workObject)
+	workObject get(studentID, workID)
+	Set<(workID, workObject)> getAllWork(studentID)
+	Set<studentID, workObject> getSubmissions(workID)
+	*/
+	public void storeSubmission(Submission submission);
 }
