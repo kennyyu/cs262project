a782fe0be62cb7a8b64f7b362d3ef571f6259abd
Kenny Yu <yu.kenny124@gmail.com>
2012-04-07 15:56:24 -0400

    added remote methods to SubmissionStorageService

--- src/edu/harvard/cs262/grading/SubmissionStorageService.java
+++ src/edu/harvard/cs262/grading/SubmissionStorageService.java
@@ -1,14 +1,48 @@
 package edu.harvard.cs262.grading;
 
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.Set;
+
 /**
  * Service for persistent storage of student work submission.
  */
-public interface SubmissionStorageService {
-	/*
-	bool put(studentID, workID, workObject)
-	workObject get(studentID, workID)
-	Set<(workID, workObject)> getAllWork(studentID)
-	Set<studentID, workObject> getSubmissions(workID)
-	*/
-	public void storeSubmission(Submission submission);
+public interface SubmissionStorageService extends Remote {
+
+	/**
+	 * Places the submission into persistent storage.
+	 * @param submission
+	 * @return true on success
+	 * @throws RemoteException
+	 */
+	public boolean storeSubmission(Submission submission)
+		throws RemoteException;
+
+	/**
+	 * Get the submission for the given student and assignment
+	 * @param studentID
+	 * @param assignmentID
+	 * @return the requested submission
+	 * @throws RemoteException
+	 */
+	public Submission getSubmission(StudentID studentID, AssignmentID assignmentID)
+		throws RemoteException;
+
+	/**
+	 * Return a set of all submitted assignments for this student
+	 * @param studentID
+	 * @return a set of all submitted assignments for this student
+	 * @throws RemoteException
+	 */
+	public Set<Submission> getStudentWork(StudentID studentID)
+		throws RemoteException;
+
+	/**
+	 * Return a set of all submitted assignments for the given assignment ID
+	 * @param assignmentID
+	 * @return
+	 * @throws RemoteException
+	 */
+	public Set<Submission> getAllSubmissions(AssignmentID assignmentID)
+		throws RemoteException;
 }
