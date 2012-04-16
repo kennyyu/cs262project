287bf48217ef7efd824a998a0f54776cbd1a0bf7
Stefan Muller <smuller@fas.harvard.edu>
2012-04-15 21:26:58 -0400

    Implemented some methods of MongoSubmissionStorageService

--- src/edu/harvard/cs262/grading/MongoSubmissionStorageService.java
+++ src/edu/harvard/cs262/grading/MongoSubmissionStorageService.java
@@ -2,8 +2,12 @@ package edu.harvard.cs262.grading;
 
 import java.net.UnknownHostException;
 import java.rmi.RemoteException;
+import java.sql.Blob;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 import com.mongodb.Mongo;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
@@ -49,22 +53,59 @@ public class MongoSubmissionStorageService implements SubmissionStorageService {
 	@Override
 	public Submission getSubmission(Student student, Assignment assignment)
 			throws RemoteException {
-		// TODO Auto-generated method stub
-		return null;
+
+		BasicDBObject query = new BasicDBObject();
+		query.put("studentID", student);
+		query.put("assignmentID", assignment);
+		
+		DBObject info = coll.findOne(query);
+		
+		Submission submission = new SubmissionImpl(student, assignment, (Blob) info.get("contents"));
+		
+		return submission;
 	}
 
 	@Override
 	public Set<Submission> getStudentWork(Student student)
 			throws RemoteException {
-		// TODO Auto-generated method stub
-		return null;
+
+		BasicDBObject query = new BasicDBObject();
+		query.put("studentID", student);
+		
+		DBCursor results = coll.find(query);
+		
+		Set<Submission> submissions = new LinkedHashSet<Submission>();
+		
+		for (DBObject result : results) {
+			Submission submission = new SubmissionImpl(student, 
+					(Assignment) result.get("assignmentID"), 
+					(Blob) result.get("contents"));
+			submissions.add(submission);
+		}
+		
+		return submissions;
 	}
 
 	@Override
 	public Set<Submission> getAllSubmissions(Assignment assignment)
 			throws RemoteException {
-		// TODO Auto-generated method stub
-		return null;
+
+		BasicDBObject query = new BasicDBObject();
+		query.put("assignmentID", assignment);
+		
+		DBCursor results = coll.find(query);
+		
+		Set<Submission> submissions = new LinkedHashSet<Submission>();
+		
+		for (DBObject result : results) {
+			Submission submission = new SubmissionImpl(
+					(Student) result.get("studentID"), 
+					assignment, 
+					(Blob) result.get("contents"));
+			submissions.add(submission);
+		}
+		
+		return submissions;
 	}
 
 	public static void main(String[] args) {
