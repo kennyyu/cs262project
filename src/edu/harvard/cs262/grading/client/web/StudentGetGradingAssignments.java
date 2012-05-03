131760577ad58024af468275e4d9f6c51c4d40fd
lpearson05 <lpearson05@gmail.com>
2012-05-03 16:24:43 -0400

    - added servlet in hopes for some method that returns grading assignments

---
+++ src/edu/harvard/cs262/grading/client/web/StudentGetGradingAssignments.java
@@ -0,0 +1,19 @@
+package edu.harvard.cs262.grading.client.web;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Returns list of graders <code>List<Student></code> for
+ * given assignment ID and student ID.
+ * 
+ * The expected posted parameters are <code>assignmentID</code>
+ * and <code>uid</code>, respectively.
+ * 
+ * @author Administrator
+ *
+ */
+public class StudentGetGradingAssignments extends HttpServlet {
+	
+	//TODO
+
+}
